083d21d17b57fa58c11a5f8e726c25f5f5f9df41
elblaus <ludvig@ludvigelblaus.com>
2018-05-23 23:32:26 +0200

    Add files via upload
    OSC-DMX bridge, contact mic analysis, and Arduino-OSC bridge

---
+++ sound-forest-arduino-osc.py
@@ -0,0 +1,18 @@
+import serial
+import OSC
+import sys
+
+port = serial.Serial(sys.argv[1], 115200)
+client = OSC.OSCClient()
+client.connect(('localhost', 57120))
+addr = '/string' + sys.argv[2] + '/distance'
+
+while True:
+    line = port.readline()
+    try:
+        message = OSC.OSCMessage()
+        message.setAddress(addr)
+        message.append(int(line))
+        client.send(message)
+    except:
+        print('No one is listening...')
